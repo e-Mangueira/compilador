0d3154ff9047a5cee186a627bf4cb159880f1fb7
e-Mangueira <nm.mangueira@ufma.br>
2024-11-15 15:41:41 -0300

    Tradudor refatorado + analisador léxico v.03

--- tradutor/src/main/java/mangueira/ecp/Main.java
+++ tradutor/src/main/java/mangueira/ecp/Main.java
@@ -1,9 +1,21 @@
 package mangueira.ecp;
 
 public class Main {
-    public static void main(String[] args) {
-        String input = "8+5-7+9";
+    public static void main(String[] args) throws Exception {
+
+        
+        String input = "8+5-7";
         Parser p = new Parser (input.getBytes());
         p.parse();
+        
+        /*
+        String input = "4-8+6";
+        Scanner scan = new Scanner (input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        */
     }
 }
