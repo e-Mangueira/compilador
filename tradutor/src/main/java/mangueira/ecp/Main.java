ef423033a2bb54a470be5bfbeb77df374e5cd71f
e-Mangueira <nm.mangueira@ufma.br>
2024-11-15 16:22:18 -0300

    Tradutor + analisador léxico - testar Scanner v. 04

--- tradutor/src/main/java/mangueira/ecp/Main.java
+++ tradutor/src/main/java/mangueira/ecp/Main.java
@@ -3,19 +3,22 @@ package mangueira.ecp;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        
+        /*
         String input = "8+5-7";
         Parser p = new Parser (input.getBytes());
         p.parse();
+        */
         
-        /*
-        String input = "4-8+6";
+        
+        String input = "289-85+0+69";
         Scanner scan = new Scanner (input.getBytes());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
-        */
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
+        
     }
 }
